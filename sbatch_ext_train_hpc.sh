775a5d246f52ff15d8a10e9ec0df88d0a0d4cbfc
Xinnuo Xu <hpcxu1@login-e-9.data.cluster>
2022-03-10 00:53:32 +0000

    ext

---
+++ sbatch_ext_train_hpc.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH -A TEAMER-SL2-GPU
+#SBATCH -p ampere
+#SBATCH --nodes=1	  # nodes requested
+#SBATCH --gres=gpu:3  # use 1 GPU
+#SBATCH --mem=14000  # memory in Mb
+#SBATCH -t 24:00:00  # time requested in hour:minute:seconds
+#SBATCH --cpus-per-task=4  # number of cpus to use - there are 32 on each node.
+#SBATCH --no-requeue
+
+numnodes=$SLURM_JOB_NUM_NODES
+numtasks=$SLURM_NTASKS
+mpi_tasks_per_node=$(echo "$SLURM_TASKS_PER_NODE" | sed -e  's/^\([0-9][0-9]*\).*$/\1/')
+. /etc/profile.d/modules.sh                # Leave this line (enables the module command)
+module purge                               # Removes all modules still loaded
+module load rhel8/default-amp              # REQUIRED - loads the basic environment
+
+set -e # fail fast
+
+dt=$(date '+%d_%m_%y_%H_%M');
+echo "I am job ${SLURM_JOB_ID}"
+echo "I'm running on ${SLURM_JOB_NODELIST}"
+echo "Job started at ${dt}"
+
+# ====================
+# Activate Anaconda environment
+# ====================
+source /home/hpcxu1/miniconda3/bin/activate Plan
+
+# ====================
+# Run training. Here we use src/gpu.py
+# ====================
+echo "Creating directory to save model weights"
+#sh ./scripts.hpc/preprocess_shard.sh
+#sh ./scripts.hpc/preprocess.sh
+sh ./scripts.hpc/train_xsum_ext.sh
+
+echo "Job ${SLURM_JOB_ID} is done!"
---
+++ scripts.hpc/train_xsum_ext.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+BERT_DATA_PATH=/home/hpcxu1/Planning/Tree_enc_dec/outputs/data/
+MODEL_PATH=/home/hpcxu1/Planning/Tree_enc_dec/outputs/models.ext/
+LOG_PATH=/home/hpcxu1/Planning/Tree_enc_dec/outputs/logs.ext/
+
+mkdir -p ${MODEL_PATH}
+mkdir -p ${LOG_PATH}
+
+python train.py  \
+	-input_path ${BERT_DATA_PATH} \
+	-model_path ${MODEL_PATH} \
+	-mode train \
+	-ext_or_abs ext \
+	-content_planning_model tree \
+        -freeze_encoder_decoder True \
+	-log_file ${LOG_PATH}/train_ext.log \
+	-train_steps 30000 \
+	-save_checkpoint_steps 5000 \
+	-warmup_steps 10000 \
+	-batch_size 3000 \
+	-report_every 50 \
+	-max_pos 1024 \
+	-max_tgt_len 250 \
+	-use_interval true \
+        -ext_dropout 0.1 \
+	-lr 2e-3 \
+	-accum_count 2 \
+	-visible_gpus 0,1,2
+
