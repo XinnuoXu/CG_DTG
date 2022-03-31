8868c49fbc27821fd756a05bab9ac43d3745f185
Xinnuo Xu <hpcxu1@gpu-q-49.data.cluster>
2022-03-31 12:52:48 +0100

    analysis tool

--- tool/statistic_tree.py
+++ tool/statistic_tree.py
@@ -7,8 +7,15 @@ import os
 if __name__ == '__main__':
     heights = []
     #for line in open('outputs.cnn_dm/logs.freeze_tmt/test.res.320000.trees_gumble'):
-    #for line in open('outputs.cnn_dm/logs.freeze_tmt/test.res.320000.trees'):
-    for line in open('outputs.cnn_dm/logs.freeze_tmt/test.res.320000.trees_free'):
+    #for line in open('outputs.cnn_dm/logs.freeze_tmt/test.res.320000.trees_free'):
+    fpout = open('./outputs.cnn_dm/logs.freeze_tmt/tree.txt', 'w')
+    for line in open('outputs.cnn_dm/logs.freeze_tmt/test.res.320000.trees'):
         tree = json.loads(line.strip())
         heights.append(tree['Height'])
+        tree_str = tree['Tree']
+        src_sents = tree['Src']
+        fpout.write(tree_str + '\n')
+        fpout.write('\n'.join(src_sents) + '\n\n')
     print (sum(heights)/len(heights))
+    fpout.close()
+    
