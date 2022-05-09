attnetion softmax bug

models/encoder.py
@@ -118,6 +118,11 @@ class StructuredAttention(nn.Module):
         L = torch.diag_embed(L)
         L = L - A
         LL = L + torch.diag_embed(R)
+        for i in range(A.size(0)):
+            try:
+                torch.inverse(LL[i])
+            except:
+                print (LL[i], A[i], R[i], scores[i], root[i], '\n')
         LL_inv = torch.inverse(LL)  # batch_l, doc_l, doc_l
         LL_inv_diag = torch.diagonal(LL_inv, 0, 1, 2)
         d0 = R * LL_inv_diag
@@ -140,6 +145,7 @@ class StructuredAttention(nn.Module):
 
         query = query / math.sqrt(self.model_dim)
         scores = torch.matmul(query, key.transpose(1, 2))
+        scores = nn.functional.normalize(scores) # not in the original code
 
         mask = mask.float()
         root = root - mask.squeeze(1) * 50
@@ -173,7 +179,6 @@ class TreeInference(nn.Module):
         sent_vec = sent_vec + global_pos_emb
 
         sent_vec = self.layer_norm2(sent_vec)* mask_block.unsqueeze(-1).float()
-        #sent_vec = self.layer_norm2(sent_vec)
         structure_vec = sent_vec
 
         roots = []; structure_vecs = []; attns = []

models/tree_reader.py
@@ -9,8 +9,7 @@ def gumbel_softmax_function(scores, tau, top_k):
     y_soft = gumbels.softmax(-1)
     top_k = min(top_k, y_soft.size(1))
     indices = torch.topk(y_soft, dim=-1, k=top_k)[1]
-    #value = 1 / top_k
-    value = 1
+    value = 1 / top_k
     y_hard = torch.zeros_like(scores.contiguous()).scatter_(-1, indices, value)
     ret = y_hard - y_soft.detach() + y_soft
     #ret = (ret == value)