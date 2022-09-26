063fa18964fab7c1ec16ea4acbefa77ccc1d818c
Xinnuo Xu <hpcxu1@login-e-13.data.cluster>
2022-09-26 15:19:00 +0100

    dbscan_clustering

--- pyrouge/run_for_ama.py
+++ pyrouge/run_for_ama.py
@@ -1,15 +1,67 @@
 #coding=utf8
 
 import sys
+import argparse
 from utils import rouge_results_to_str, test_rouge
 from nltk import word_tokenize
-#import nltk.data
-#tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
 TEMP_DIR = './temp/'
-CAN_PATH = './outputs.ama/logs.summarizer/test.res.120000.candidate'
-GOLE_PATH = './outputs.ama/logs.summarizer/test.res.120000.gold'
-EID_PATH = './outputs.ama/logs.summarizer/test.res.120000.eid'
+
+def str2bool(v):
+	if v.lower() in ('yes', 'true', 't', 'y', '1'):
+		return True
+	elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+		return False
+	else:
+		raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
+def process_system_output(candidate_path, eid_path):
+	candidates = [' '.join(word_tokenize(line.strip())) for line in open(candidate_path)]
+	eids = [line.strip() for line in open(eid_path)]
+	zipped_pairs = zip(eids, candidates)
+
+	id_to_sentences = {}
+	for pair in zipped_pairs:
+		eid = pair[0]
+		cand = pair[1]
+
+		eid_list = eid.split('_')
+		example_id = '_'.join(eid_list[:2])
+		tag = eid_list[-1]
+		line_id = example_id + '_' + tag
+	
+		cand = ':'.join(cand.split(':')[1:]).strip()
+		if line_id not in id_to_sentences:
+			id_to_sentences[line_id] = cand
+		else:
+			id_to_sentences[line_id] += (' ' + cand)
+	
+	return id_to_sentences
+
+
+def process_cluster_clean(gold_path, eid_path):
+	golds = [line.strip() for line in open(gold_path)]
+	eids = [line.strip() for line in open(eid_path)]
+	zipped_pairs = zip(eids, golds)
+
+	id_to_sentences = {}
+	for pair in zipped_pairs:
+		eid = pair[0]
+		gold = pair[1]
+
+		eid_list = eid.split('_')
+		example_id = '_'.join(eid_list[:2])
+		tag = eid_list[-1]
+		line_id = example_id + '_' + tag
+	
+		if line_id not in id_to_sentences:
+			id_to_sentences[line_id] = gold
+		else:
+			id_to_sentences[line_id] += (' ' + gold)
+	
+	return id_to_sentences
+
 
 def run_one_category(categroy, pairs):
 
@@ -29,32 +81,16 @@ def run_one_category(categroy, pairs):
 
 	return test_rouge(TEMP_DIR, cand_path, gold_path)
 
-if __name__ == '__main__':
-	candidates = [' '.join(word_tokenize(line.strip())) for line in open(CAN_PATH)]
-	golds = [line.strip() for line in open(GOLE_PATH)]
-	eids = [line.strip() for line in open(EID_PATH)]
-	triples = zip(eids, golds, candidates)
+
+def run_pyrouge(id_to_cands, id_to_golds):
 
 	pairs = {}
-	for triple in triples:
-		eid = triple[0]
-		gold = triple[1]
-		cand = triple[2]
+	for idx in id_to_cands:
+		cand = id_to_cands[idx]
+		gold = id_to_golds[idx]
+		pairs[idx] = [gold, cand]
 
-		eid_list = eid.split('_')
-		example_id = '_'.join(eid_list[:2])
-		tag = cand.split(' ')[0]
-		line_id = example_id + '_' + tag
-	
-		#cand = cand.split(' : ')[1]
-		if line_id not in pairs:
-			pairs[line_id] = [gold, cand]	
-		else:
-			pairs[line_id][0] += (' ' + gold)
-			pairs[line_id][1] += (' ' + cand)
-	
 	pros_results_dict = run_one_category('pros', pairs)
-	'''
 	cons_results_dict = run_one_category('cons', pairs)
 	verdict_results_dict = run_one_category('verdict', pairs)
 
@@ -65,4 +101,24 @@ if __name__ == '__main__':
 	print ('CONS results')
 	print (rouge_results_to_str(cons_results_dict))
 
-	'''
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-gold_type", default='cluster_clean', type=str, choices=['raw', 'clean', 'cluster_clean'])
+	parser.add_argument("-gold_path", default='', type=str)
+	parser.add_argument("-cand_type", default='systems', type=str, choices=['systems', 'selsum'])
+	parser.add_argument("-cand_path", default='', type=str)
+	parser.add_argument("-eid_path", default='', type=str)
+	args = parser.parse_args()
+
+	# Process system outputs
+	if args.cand_type == 'systems':
+		id_to_cands = process_system_output(args.cand_path, args.eid_path)
+	
+	# Process golds
+	if args.gold_type == 'cluster_clean':
+		id_to_golds = process_cluster_clean(args.gold_path, args.eid_path)
+
+	# Run pyrouge
+	run_pyrouge(id_to_cands, id_to_golds)
---
+++ pyrouge/run_for_ama.sh
@@ -0,0 +1,8 @@
+
+python pyrouge/run_for_ama.py \
+	-gold_type cluster_clean \
+	-gold_path ./outputs.ama/logs.summarizer/test.res.120000.gold \
+	-cand_type systems \
+	-cand_path ./outputs.ama/logs.summarizer/test.res.120000.candidate \
+	-eid_path ./outputs.ama/logs.summarizer/test.res.120000.eid \
+
